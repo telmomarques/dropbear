6791ef8a7da621daf8b38ee9d7e43dbebe8178f1
Telmo Marques <telmofmm@gmail.com>
2020-05-13 15:56:21 -0400

    Initial import

---
+++ localoptions.h
@@ -0,0 +1,6 @@
+#define DSS_PRIV_FILENAME "/mnt/data/etc/dropbear/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/mnt/data/etc/dropbear/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/mnt/data/etc/dropbear/dropbear_ecdsa_host_key"
+
+#define DROPBEAR_SFTPSERVER 1
+#define SFTPSERVER_PATH "/mnt/sdcard/hacks/sftp-server/bin/sftp-server"
